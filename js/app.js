3946a98583d9a7659db86d0e5c9c97bfd520c0fa
Hanna <hannaeberhoefer@yahoo.com>
2022-03-25 20:18:04 +0000

    active section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 // Define variables
 
-// const navbar = document.querySelector(".navbar__list");
 const navbar = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
 
@@ -26,3 +25,21 @@ for (const section of sections) {
   navbar.appendChild(navbarItem);
   scroll(navbarLink, section);
 }
+
+// Add active class
+
+window.addEventListener("scroll", () => addActiveClass());
+
+const addActiveClass = () => {
+  let scrollY = window.pageYOffset;
+  for (const section of sections) {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 200;
+    sectionId = section.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      section.classList.add("active");
+    } else {
+      section.classList.remove("active");
+    }
+  }
+};
