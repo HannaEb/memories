fb014b1efff3f2d231ce48255e9d92eb88a62d95
Hanna <hannaeberhoefer@yahoo.com>
2022-03-28 19:57:32 +0100

    image texture

--- js/index.js
+++ js/index.js
@@ -6,18 +6,32 @@ const init = () => {
   const canvas = document.querySelector("#canvas");
   const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 
-  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
-  camera.position.z = 2;
+  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100);
+  camera.position.z = 25;
 
   const scene = new THREE.Scene();
 
-  const geometry = new THREE.SphereGeometry(1, 100, 100);
-  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  {
+    const light = new THREE.DirectionalLight(0xffffff, 1); // color & intensity
+    light.position.set(100, 50, 100);
+    scene.add(light);
+  }
+
+  const geometry = new THREE.SphereGeometry(10, 100, 100);
+  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+
+  material.map = THREE.ImageUtils.loadTexture("images/world-map1.jpg");
 
   const sphere = new THREE.Mesh(geometry, material);
+  sphere.rotation.x += 0.2;
   scene.add(sphere);
 
-  renderer.render(scene, camera);
+  const render = () => {
+    sphere.rotation.y += 0.005;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
 };
 
 init();
