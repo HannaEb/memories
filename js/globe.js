f5d5a745527b07e68ffb5dcc384f4f338f61f9e8
Hanna <hannaeberhoefer@yahoo.com>
2022-03-28 20:13:00 +0100

    create globe.js

---
+++ js/globe.js
@@ -0,0 +1,53 @@
+import * as THREE from "../vendor/three/build/three.module.js";
+
+// Create spinning globe
+
+const init = () => {
+  const canvas = document.querySelector("#canvas");
+  const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+
+  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100);
+  camera.position.z = 25;
+
+  const scene = new THREE.Scene();
+
+  {
+    const light = new THREE.DirectionalLight(0xffffff, 1); // color & intensity
+    light.position.set(100, 50, 100);
+    scene.add(light);
+  }
+
+  const geometry = new THREE.SphereGeometry(10, 100, 100);
+  const material = new THREE.MeshPhongMaterial({ color: 0xffffff });
+
+  material.map = THREE.ImageUtils.loadTexture("images/world-map.jpg");
+
+  const sphere = new THREE.Mesh(geometry, material);
+  sphere.rotation.x += 0.2;
+  scene.add(sphere);
+
+  function resize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  const render = () => {
+    if (resize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    sphere.rotation.y += 0.005;
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
+};
+
+init();
